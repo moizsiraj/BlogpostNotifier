cd8a8f79529a5f9e9d71dafdea2a3dad6af674ea
Abdul Moiz Siraj <abdul.moiz@astera.com>
2021-07-18 17:57:36 +0500

    blogpost class added

---
+++ Task_1/BlogPost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_1 {
+    class BlogPost {
+
+        public BlogPost(int id, string text) {
+            
+            _Id = id;
+            _Text = text;
+ 
+        }
+
+        private int _Id;
+
+        public int Id {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+
+        private string _Text;
+
+        public string Text {
+            get { return _Text; }
+            set { _Text = value; }
+        }
+
+        public void EditPost(string text) {
+            _Text = text;
+        }
+
+        public static void DeletePost(BlogPost blogPost) {
+            blogPost = null;
+        }
+
+
+    }
+}
